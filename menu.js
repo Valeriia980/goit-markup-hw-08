8855fb0b1e90a8a2e8f0989c6605d74a29e8351f
Valeriia980 <75097633+Valeriia980@users.noreply.github.com>
2021-02-07 23:07:18 +0100

    Add files via upload

--- menu.js
+++ menu.js
@@ -6,9 +6,9 @@
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-    menuBtnRef.classList.toggle("open-menu");
+    menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
-    mobileMenuRef.classList.toggle("open-menu");
+    mobileMenuRef.classList.toggle("is-open");
   });
 })();
